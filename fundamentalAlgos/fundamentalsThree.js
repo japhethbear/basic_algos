902bcf33f773b2daa33e76b885251ca2f2baeced
japhethbear <japhethbear@gmail.com>
2023-04-10 22:12:52 -0500

    More array algos

--- fundamentalAlgos/fundamentalsThree.js
+++ fundamentalAlgos/fundamentalsThree.js
@@ -17,8 +17,6 @@
 
 // Print Low, Return High
 
-let randomArray = [41, 75, 99, 1110, 42, 58, 7, -14, 4]
-
 // const printLowReturnHigh = (array) => {
 //     let highest = array[0]
 //     let lowest = array[0]
@@ -84,29 +82,74 @@ let randomArray = [41, 75, 99, 1110, 42, 58, 7, -14, 4]
 
 // Evens and Odds
 
-const threesCompany = array => {
-    let consecutiveOdds = 0;
-    let consecutiveEvens = 0;
+// const threesCompany = array => {
+//     let consecutiveOdds = 0;
+//     let consecutiveEvens = 0;
       
-        for (let i = 0; i < array.length; i++) {
-          if (array[i] % 2 !== 0) {
-            consecutiveOdds++;
-            consecutiveEvens = 0;
-            if (consecutiveOdds === 3) {
-              console.log("That's odd!");
-              consecutiveOdds = 0;
-            }
-          } else {
-            consecutiveEvens++;
-            consecutiveOdds = 0;
-            if (consecutiveEvens === 3) {
-              console.log("Even more so!");
-              consecutiveEvens = 0;
-            }
-          }
-        }
-      }
+//         for (let i = 0; i < array.length; i++) {
+//           if (array[i] % 2 !== 0) {
+//             consecutiveOdds++;
+//             consecutiveEvens = 0;
+//             if (consecutiveOdds === 3) {
+//               console.log("That's odd!");
+//               consecutiveOdds = 0;
+//             }
+//           } else {
+//             consecutiveEvens++;
+//             consecutiveOdds = 0;
+//             if (consecutiveEvens === 3) {
+//               console.log("Even more so!");
+//               consecutiveEvens = 0;
+//             }
+//           }
+//         }
+//       }
 
-threesCompany(randomArray)
+// threesCompany(randomArray)
+
+// Increment the Seconds
+
+let randomArray = [41, 75, 99, 1110, 42, 58, 7, -14, 4]
+const incrementSeconds = array => {
+    for (let i = 0; i < array.length; i++) {
+        if (i % 2 !== 0) {
+            array[i] += 1;
+        }
+        console.log(array[i]);
+    }
+    return array
+}
+
+let modified_Array = incrementSeconds(randomArray)
+
+console.log(modified_Array);
+
+// Previous Lengths
+
+
+
+// Add Seven to Most
+
+
+
+// Reverse Array
+
+
+
+// Outlook: Negative
+
+
+
+// Always Hungry
+
+
+
+// Swap Toward the Center
+
+
+
+// Scale the Array
+
+
 
 // 
